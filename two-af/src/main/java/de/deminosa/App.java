9cb378f7bbbc7f6f8042d123b74893c547fa21b5
Deminosa <admin@deminosa.de>
2023-01-11 20:55:15 +0100

    Webinterface Base module (With Error)

--- two-af/src/main/java/de/deminosa/App.java
+++ two-af/src/main/java/de/deminosa/App.java
@@ -3,14 +3,21 @@ package de.deminosa;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class App extends Plugin{
+
+    private static App instance;
     
     @Override
     public void onEnable() {
+        instance = this;
         super.onEnable();
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public static App getInstance() {
+        return instance;
     }
 }
--- two-af/src/main/java/de/deminosa/web/Webinterface.java
+++ two-af/src/main/java/de/deminosa/web/Webinterface.java
@@ -1,36 +1,45 @@
 package de.deminosa.web;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import de.deminosa.App;
 import de.deminosa.web.response.QueryResponse;
 
-public class Webinterface {
+public class Webinterface implements HttpHandler{
     
-    private static WebServerManager webServerManager;
-	private static int port;
+    private final WebServerManager webServerManager;
+	private final int port;
 
     private final List<QueryResponse> queryResponse;
 
     public Webinterface() {
         queryResponse = new ArrayList<>();
+        port = 20000;
+		webServerManager = new WebServerManager(port);
     }
 
     public void onEnable() {
-		port = 20000;
-		webServerManager = new WebServerManager(port);
 		webServerManager.start();
+
+        loadFiles();
 	}
 
     public void onDisable() {
         webServerManager.stop();
     }
 
-    public static WebServerManager getWebServerManager() {
+    public WebServerManager getWebServerManager() {
 		return webServerManager;
 	}
 	
-	public static int getCurrentPort() {
+	public int getCurrentPort() {
 		return port;
 	}
 	
@@ -43,5 +52,35 @@ public class Webinterface {
 	public List<QueryResponse> getQueryResponse() {
 		return queryResponse;
 	}
+
+    private void loadFiles() {
+        File dir = new File(App.getInstance().getDataFolder() + "//webpages//");
+        if(!dir.getParentFile().exists()) {
+            dir.mkdirs();
+        }
+
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+
+        File[] files = dir.listFiles();
+        if(files == null) return;
+
+        for(int i = 0; i < files.length; i++){
+            if(!files[i].isDirectory()){
+                /* TODO: Why VSCode?
+                 * 
+                 * The method getServer() from the type WebServerManager refers to the missing type HttpServer // Java(67108984)
+                 */
+                webServerManager.getServer().createContext("/" + files[i].getName(), this);
+            }
+        }
+    }
+
+    @Override
+    public void handle(HttpExchange arg0) throws IOException {
+        
+    }
+
 
 }
